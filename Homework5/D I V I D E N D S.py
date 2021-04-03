8d66241890dc9f2b2daa28fec90caed454e59227
Your Name <you@example.com>
2021-04-03 20:37:10 +0400

    Homework5

--- Homework5/D I V I D E N D S.py
+++ Homework5/D I V I D E N D S.py
@@ -1,4 +1,4 @@
-numbers = [12, 24, 42, "test", "True", 43, 34, "a", 72]
+numbers = [i for i in input("Input dividends ").split()]
 n = int(input("Input any number "))
-for i in filter(lambda x: isinstance(x, int) and x % n == 0, numbers):
-    print(i)
+for i in filter(lambda x: x.isnumeric() and int(x) % n == 0, numbers):
+    print(i, end=" ")
